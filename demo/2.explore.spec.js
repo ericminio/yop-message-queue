8e1a1277fdea817a6a065c3b88c3c474caf9adcf
Eric Mignot <eric.mignot@gmail.com>
2019-03-22 15:00:19 -0700

    several clients, subject filtering

--- demo/2.explore.spec.js
+++ demo/2.explore.spec.js
@@ -2,40 +2,54 @@ const { expect } = require('chai')
 const { request, createServer } = require('http')
 const { queue } = require('..')
 
+var Client = function(options) {
+    this.port = options.port
+    this.server = createServer((request, response)=>{
+        var body = '';
+        request.on('data', (chunk) => {
+            body += chunk;
+        });
+        request.on('end', () => {
+            this.received = JSON.parse(body)
+            response.end()
+        });
+    })
+}
+Client.prototype.start = function(done) {
+    this.server.listen(this.port, done)
+}
+Client.prototype.stop = function(done) {
+    this.server.close(done)
+}
+
+var registration = function(port) {
+    return {
+        path:'/register?subject=weather&host=localhost&path=/&port='+port,
+        method:'POST',
+        port:queue.port
+    }
+}
+var notification = {
+    path:'/notify?subject=weather&message=clear',
+    method:'POST',
+    port:queue.port
+}
+
 describe('message queue', ()=>{
 
-    var client = {}
+    var client
     beforeEach((done)=>{
-        client.server = createServer((request, response)=>{
-            var body = '';
-            request.on('data', (chunk) => {
-                body += chunk;
-            });
-            request.on('end', () => {
-                client.received = JSON.parse(body)
-                response.end()
-            });
-        })
-        client.server.listen(5003, done)
+        client = new Client({ port:5003 })
+        client.start(done)
     })
     afterEach((done)=>{
-        client.server.close(done)
+        client.stop(done)
     })
 
     it('accepts registration', (done)=>{
-        var registration = {
-            path:'/register?subject=weather&host=localhost&port=5003&path=/',
-            method:'POST',
-            port:queue.port
-        }
-        var register = request(registration, (response)=>{
+        var register = request(registration(5003), (response)=>{
             expect(response.statusCode).to.equal(201)
 
-            var notification = {
-                path:'/notify?subject=weather&message=clear',
-                method:'POST',
-                port:queue.port
-            }
             var notify = request(notification, (response)=>{
                 expect(response.statusCode).to.equal(201)
 
@@ -45,5 +59,60 @@ describe('message queue', ()=>{
             notify.end()
         })
         register.end()
+    })
+    it('does not notify unregistered subject', (done)=>{
+        var register = request(registration(5003), (response)=>{
+            expect(response.statusCode).to.equal(201)
+            var notification = {
+                path:'/notify?subject=other&message=any',
+                method:'POST',
+                port:queue.port
+            }
+            var notify = request(notification, (response)=>{
+                expect(response.statusCode).to.equal(201)
+
+                expect(client.received).to.deep.equal(undefined)
+                done()
+            })
+            notify.end()
+        })
+        register.end()
+    })
+
+    describe('several clients', ()=>{
+
+        var one, two
+        beforeEach((done)=>{
+            queue.clearRegistrations()
+            one = new Client({ port:5011 })
+            one.start(()=>{
+                two = new Client({ port:5012 })
+                two.start(done)
+            })
+        })
+        afterEach((done)=>{
+            one.stop(()=>{
+                two.stop(done)
+            })
+        })
+
+        it('can register to the same subject', (done)=>{
+            var register = request(registration(5011), (response)=>{
+                var register = request(registration(5012), (response)=>{
+                    expect(response.statusCode).to.equal(201)
+
+                    var notify = request(notification, (response)=>{
+                        expect(response.statusCode).to.equal(201)
+
+                        expect(one.received).to.deep.equal({ subject:'weather', message:'clear' })
+                        expect(two.received).to.deep.equal({ subject:'weather', message:'clear' })
+                        done()
+                    })
+                    notify.end()
+                })
+                register.end()
+            })
+            register.end()
+        })
     })
 })
--- index.js
+++ index.js
@@ -1,38 +1,57 @@
 const { createServer, request } = require('http')
-const url = require('url')
+const { parse } = require('url')
+const { Promise, Promises } = require('yop-promises')
+
+var registrations = []
 
 const port = 5005
-const clients = []
 const queue = {
+    clearRegistrations: ()=>{
+        registrations = []
+    },
     start: (callback)=>{
         this.internal = createServer((incoming, response)=>{
-            var called = url.parse(incoming.url, true)
+            var called = parse(incoming.url, true)
             if ('/register' === called.pathname) {
-                clients.push({
-                    host: called.query.host,
-                    port: parseInt(called.query.port),
-                    path: called.query.path
+                registrations.push({
+                    subject: called.query.subject,
+                    client: {
+                        host: called.query.host,
+                        port: parseInt(called.query.port),
+                        path: called.query.path
+                    }
                 })
                 response.writeHead(201)
                 response.end()
             }
             if ('/notify' === called.pathname) {
-                var client = clients[0]
-                var notification = {
-                    method:'POST',
-                    host: client.host,
-                    port: client.port,
-                    path: client.path
-                }
-                var notify = request(notification, (r)=>{
+                var ps = new Promises()
+                ps.done(()=>{
                     response.writeHead(201)
                     response.end()
                 })
-                notify.write(JSON.stringify({
-                    subject:called.query.subject,
-                    message:called.query.message
-                }))
-                notify.end()
+                for (var i=0; i<registrations.length; i++) {
+                    var client = registrations[i]
+                    if (client.subject == called.query.subject) {
+                        var p = new Promise()
+                        ps.waitFor(p)
+                        var notification = {
+                            method:'POST',
+                            host: client.client.host,
+                            port: client.client.port,
+                            path: client.client.path
+                        }
+                        var notify = request(notification, (r)=>{
+                            p.resolve()
+                        })
+                        notify.write(JSON.stringify({
+                            subject:called.query.subject,
+                            message:called.query.message
+                        }))
+                        notify.end()
+                    }
+                }
+                if (ps.promises.length == 0) { ps.resolveCallback() }
             }
         })
         this.internal.listen(port, callback)
