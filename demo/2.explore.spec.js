df43d1e4641b6a9daa692fc47bdaa7e7cc6eeb15
Eric Mignot <eric.mignot@gmail.com>
2019-03-22 15:02:14 -0700

    extract demo/support/client

--- demo/2.explore.spec.js
+++ demo/2.explore.spec.js
@@ -1,26 +1,7 @@
 const { expect } = require('chai')
 const { request, createServer } = require('http')
 const { queue } = require('..')
-
-var Client = function(options) {
-    this.port = options.port
-    this.server = createServer((request, response)=>{
-        var body = '';
-        request.on('data', (chunk) => {
-            body += chunk;
-        });
-        request.on('end', () => {
-            this.received = JSON.parse(body)
-            response.end()
-        });
-    })
-}
-Client.prototype.start = function(done) {
-    this.server.listen(this.port, done)
-}
-Client.prototype.stop = function(done) {
-    this.server.close(done)
-}
+const { Client } = require('./support/client')
 
 var registration = function(port) {
     return {
